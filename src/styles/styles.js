1a9156501287827fed65e1a38aa230dfc8bebaf9
tuan908 <tuan.a1.bt@gmail.com>
2021-07-01 23:15:29 +0700

    React + Webpack for basic usage

---
+++ src/styles/styles.js
@@ -0,0 +1,10 @@
+const { default: styled } = require("styled-components");
+
+const Container = styled.div`
+  div {
+    background-color: #110111;
+    color: white;
+  }
+`;
+
+export { Container };
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const webpack = require("webpack");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
+
+const VENDOR_LIBS = ["react", "react-dom"];
+
+const config = {
+  mode: "development",
+  entry: {
+    bundle: "./src/index.js",
+    vendor: VENDOR_LIBS,
+  },
+  output: {
+    filename: "[name].[chunkhash].js",
+    path: path.join(__dirname, "build"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        use: "babel-loader",
+        test: /\.(js|jsx)$/,
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        loader: "file-loader",
+        test: /\.(jpe?g|gif|png|svg|woff|woff2|eot|ttf|wav|mp3|m4a|ico)$/,
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new webpack.optimize.SplitChunksPlugin({
+      name: "vendor",
+    }),
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+    new WebpackManifestPlugin(),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "build"),
+    compress: true,
+    port: 3000,
+    hot: true,
+  },
+  optimization: {
+    moduleIds: "deterministic",
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
+};
+
+module.exports = config;
